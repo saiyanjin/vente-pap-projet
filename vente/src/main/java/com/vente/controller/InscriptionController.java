dd48c7233fdaec0fbd0206a1b38fad6979c22f8f
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2025-01-20 18:44:17 +0100

    sdf

--- vente/src/main/java/com/vente/controller/InscriptionController.java
+++ vente/src/main/java/com/vente/controller/InscriptionController.java
@@ -36,7 +36,7 @@ public class InscriptionController {
             return "inscription";
         }
         
-        return "index";  // Redirection vers l'ajout d'objet
+        return "login";
     }
 
 
